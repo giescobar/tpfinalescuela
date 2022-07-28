6ac76312b62008db2f76604d364de7c2c01b5de9
giescobar <109322135+giescobar@users.noreply.github.com>
2022-07-28 20:24:01 -0300

    Add files via upload
    trabajo final

---
+++ mijavascript.js
@@ -0,0 +1,12 @@
+window.alert ("Te damos la bienvenida");
+
+let edad; 
+edad = prompt("¿Qué edad tienes?: ");
+console.log (edad);
+if(edad >=18) { 
+    alert("Puedes acceder");
+    } else {
+    alert("Te sugerimos el acompañamiento de un adulto mayor");
+    }
+console.log (edad);
+
